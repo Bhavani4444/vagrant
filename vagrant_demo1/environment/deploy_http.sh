724084f29450b06094824f41a3f0e5f76659cc33
Bhavani <bhavanigcjuly@gmail.com>
2021-10-13 19:40:40 +0530

    added vagrant_demo and vagrant_demo1 folders

---
+++ vagrant_demo1/environment/deploy_http.sh
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<div style="position:relative;">
+  <div style="opacity:0.5;position:absolute;left:50px;top:-30px;width:300px;height:150px;background-color:#40B3DF"></div>
+  <div style="opacity:0.3;position:absolute;left:120px;top:20px;width:100px;height:170px;background-color:#73AD21"></div>
+  <div style="margin-top:30px;width:460px;height:430px;padding:20px;border-radius:10px;border:10px solid #EE872A;font-size:120%;">
+    <h1>Hellow Guys Welcome to devops session</h1>
+    <div style="letter-spacing:1px;font-size:20px;position:relative;left:25px;top:25px;">vagrant Demo</div>
+    <div style="color:#40B3DF;letter-spacing:12px;font-size:15px;position:relative;left:25px;top:30px;">Automation World,
+    <span style="background-color:#B4009E;color:#ffffff;"> Think big in your deployment scenario</span></div>
+    <h1>your goal is your success</h1>
+  </div>
+</div>
+
+</body>
+</html>
